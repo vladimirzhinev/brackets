613334e67aedf60dccf7b556bc528c4362112b63
vladimirzhinev <vladimirzhinev@gmail.com>
2022-01-30 07:01:50 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,37 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+   let arrOpenBrackets = [];
+
+   let arrCloseBrackets = [];
+
+   let stack = [];
+
+   bracketsConfig.forEach(it => it.forEach((it, ind) => { if (ind == 0) { arrOpenBrackets.push(it) } }))
+   bracketsConfig.forEach(it => it.forEach((it, ind) => { if (ind == 1) { arrCloseBrackets.push(it) } }))
+
+   for (let i = 0; i < str.length; i++) {
+      let currSymb = str[i]
+      let topEl = stack[stack.length - 1];
+
+      if (topEl === currSymb && arrCloseBrackets.includes(currSymb)) {
+         stack.pop();
+      } else {
+
+         if (arrOpenBrackets.includes(currSymb)) {
+            stack.push(currSymb);
+         }
+
+         else if (arrCloseBrackets.includes(currSymb) && stack.length === 0) {
+            return false;
+         }
+
+         else {
+            if (arrCloseBrackets.includes(currSymb)) {
+               stack.pop();
+            }
+
+         }
+      }
+      // console.log(stack);
+   }
+   return stack.length === 0;
 }
